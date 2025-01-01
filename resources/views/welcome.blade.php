2c5ddbed387e6a4e0acc077349ed8636e8ad00c4
dudurb17 <eduardo.robettibedin@gmail.com>
2025-01-01 17:07:03 -0300

    created header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Minimal E-commerce-app</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    @vite('resources/css/app.css')
 
     <style></style>
 </head>
 
 <body>
-
+    <livewire:header />
 </body>
 
 </html>
