4cdae8af29ffd0b833d9eec83f9e6b87d215c6ca
dudurb17 <eduardo.robettibedin@gmail.com>
2025-01-02 15:41:51 -0300

    adding breadcrumb on admin dashboard

--- app/Livewire/AdminDashboard.php
+++ app/Livewire/AdminDashboard.php
@@ -4,10 +4,20 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+
 class AdminDashboard extends Component
 {
+    public $currentUrl;
+
     public function render()
     {
+        // dd(url()->current());
+        $current_url = url()->current();
+        $explode_url = explode('/', $current_url);
+
+        $this->currentUrl = $explode_url[3] . ' ' . $explode_url[4];
+
+        // dd($explode_url);
         return view('livewire.admin-dashboard')->layout('admin-layout');
     }
 }
--- resources/views/livewire/admin-dashboard.blade.php
+++ resources/views/livewire/admin-dashboard.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <livewire:bread-crumb :url="$currentUrl" />
 </div>
