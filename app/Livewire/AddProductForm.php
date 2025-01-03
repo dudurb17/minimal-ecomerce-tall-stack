46521a976aeef911fffe77568662198946c7b700
dudurb17 <eduardo.robettibedin@gmail.com>
2025-01-02 21:11:09 -0300

    created style of view table of products

---
+++ app/Livewire/AddProductForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AddProductForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-product-form')->layout('admin-layout');
+    }
+}
