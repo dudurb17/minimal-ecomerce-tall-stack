734bcd9d1278b5626d7200365bf4c6365f66b61a
dudurb17 <eduardo.robettibedin@gmail.com>
2025-01-03 15:58:33 -0300

    adjusted table of category

--- resources/views/livewire/manage-categories.blade.php
+++ resources/views/livewire/manage-categories.blade.php
@@ -1,5 +1,6 @@
+<!-- Table Section -->
 <div>
-    <!-- Table Section -->
+    <livewire:bread-crumb :url="$currentUrl" />
     <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <!-- Card -->
         <div class="flex flex-col">
@@ -12,104 +13,103 @@
                             class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-neutral-700">
                             <div>
                                 <h2 class="text-xl font-semibold text-gray-800 dark:text-neutral-200">
-                                    Category
+                                    category
                                 </h2>
                                 <p class="text-sm text-gray-600 dark:text-neutral-400">
-                                    Add Category, edit and more.
+                                    Add category, edit and more.
                                 </p>
                             </div>
 
-                            <div>
-                                <div class="inline-flex gap-x-2">
-                                    <a class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
-                                        href="#">
-                                        View all
-                                    </a>
-
-                                    <a wire:navigate
-                                        class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-                                        href="/add/category">
-                                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
-                                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="M5 12h14" />
-                                            <path d="M12 5v14" />
-                                        </svg>
-                                        Add Category
-                                    </a>
+                            <div class="inline-flex gap-x-2">
+                                <div class="max-w-md space-y-3">
+                                    <input type="search" wire:model.live.debounce.300="search"
+                                        class="peer py-3 px-4 block w-full bg-gray-100 border-blue-500 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                                        placeholder="Search category">
                                 </div>
+
+                                <a wire:navigate
+                                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                                    href="/add/category">
+                                    <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
+                                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M5 12h14" />
+                                        <path d="M12 5v14" />
+                                    </svg>
+                                    Add category
+                                </a>
                             </div>
                         </div>
                         <!-- End Header -->
 
                         <!-- Table -->
                         <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
-                            <thead class="bg-gray-50 dark:bg-neutral-800">
+                            <thead class="bg-gray-50 dark:bg-neutral-800  px-5">
                                 <tr>
 
-                                    <th scope="col" class="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3 text-start px-5 ">
-                                        <div class="flex items-center gap-x-2">
-                                            <span
-                                                class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
-                                                Product Name
-                                            </span>
-                                        </div>
-                                    </th>
+                                    @include('livewire.theaders.th', [
+                                        'name' => 'name',
+                                        'columnName' => 'category Name',
+                                    ])
 
-
-                                    <th scope="col" class="px-6 py-3 text-start">
-                                        <div class="flex items-center gap-x-2">
-                                            <span
-                                                class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
-                                                Created
-                                            </span>
-                                        </div>
-                                    </th>
-
-
-
+                                    @include('livewire.theaders.th', [
+                                        'name' => 'created_at', //column name from db
+                                        'columnName' => 'Created', //display name
+                                    ])
+                                    <th scope="col" class="px-6 py-3 text-end"></th>
                                     <th scope="col" class="px-6 py-3 text-end"></th>
                                 </tr>
                             </thead>
 
                             <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
-                                <tr>
+                                @if (count($categories) > 0)
+                                    @foreach ($categories as $category)
+                                        <tr wire:key="{{ $category->id }}">
+                                            <td class="size-px  px-5">
+                                                <div class="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
+                                                    <div class="grow">
+                                                        <span
+                                                            class="block text-sm font-semibold text-gray-800 dark:text-neutral-200">{{ str($category->name)->words(3) }}</span>
+                                                    </div>
 
-
-
-
-                                    <td class="size-px whitespace-nowrap">
-                                        <div class="px-6 py-3">
-                                            <div class="flex items-center gap-x-3">
-                                                <span class="text-xs text-gray-500 dark:text-neutral-500">1/5</span>
-                                                <div
-                                                    class="flex w-full h-1.5 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700">
-                                                    <div class="flex flex-col justify-center overflow-hidden bg-gray-800 dark:bg-neutral-200"
-                                                        role="progressbar" style="width: 25%" aria-valuenow="25"
-                                                        aria-valuemin="0" aria-valuemax="100"></div>
                                                 </div>
+                                            </td>
+                                            <td class="size-px whitespace-nowrap">
+                                                <div class="px-6 py-3">
+                                                    <span
+                                                        class="text-sm text-gray-500 dark:text-neutral-500">{{ date('D M Y, H:i', strtotime($category->created_at)) }}</span>
+                                                </div>
+                                            </td>
+                                            <td class="size-px whitespace-nowrap">
+                                                <div class="px-6 py-1.5">
+                                                    <a class="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500"
+                                                        href="#">
+                                                        Edit
+                                                    </a>
+                                                </div>
+                                            </td>
+                                            <td class="size-px whitespace-nowrap">
+                                                <div class="px-6 py-1.5">
+                                                    <a class="inline-flex items-center gap-x-1 text-sm text-red-500 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500"
+                                                        href="#">
+                                                        Delete
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td class="size-px whitespace-nowrap" colspan="5">
+                                            <div class="px-6 py-3">
+                                                <span
+                                                    class="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500">
+                                                    No Data Found!
+                                                </span>
                                             </div>
-                                        </div>
-                                    </td>
-                                    <td class="size-px whitespace-nowrap">
-                                        <div class="px-6 py-3">
-                                            <span class="text-sm text-gray-500 dark:text-neutral-500">28 Dec,
-                                                12:12</span>
-                                        </div>
-                                    </td>
-                                    <td class="size-px whitespace-nowrap">
-                                        <div class="px-6 py-1.5">
-                                            <a class="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500"
-                                                href="#">
-                                                Edit
-                                            </a>
-                                        </div>
-                                    </td>
-                                </tr>
-
-
-
-
+                                        </td>
+                                    </tr>
+                                @endif
                             </tbody>
                         </table>
                         <!-- End Table -->
@@ -117,35 +117,22 @@
                         <!-- Footer -->
                         <div
                             class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-neutral-700">
+                            <div class="flex gap-2">
+                                <label class="w-32 text-sm font-medium text-gray-900">Per Page</label>
+                                <select wire:model.live='perPage'
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                                    <option value="5">5</option>
+                                    <option value="7">10</option>
+                                    <option value="20">20</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select>
+                            </div>
+                            <!-- the links to different pages -->
                             <div>
-                                <p class="text-sm text-gray-600 dark:text-neutral-400">
-                                    <span class="font-semibold text-gray-800 dark:text-neutral-200">12</span> results
-                                </p>
+                                {{ $categories->links() }}
                             </div>
 
-                            <div>
-                                <div class="inline-flex gap-x-2">
-                                    <button type="button"
-                                        class="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
-                                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
-                                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="m15 18-6-6 6-6" />
-                                        </svg>
-                                        Prev
-                                    </button>
-
-                                    <button type="button"
-                                        class="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
-                                        Next
-                                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
-                                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="m9 18 6-6-6-6" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
                         </div>
                         <!-- End Footer -->
                     </div>
@@ -154,6 +141,6 @@
         </div>
         <!-- End Card -->
     </div>
-    <!-- End Table Section -->
-
 </div>
+
+<!-- End Table Section -->
