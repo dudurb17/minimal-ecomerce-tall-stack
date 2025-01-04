89cfcc3c216a86301a2a30fc43dc8a951de72fa1
dudurb17 <eduardo.robettibedin@gmail.com>
2025-01-04 14:08:31 -0300

    created loading skeleton

--- app/Livewire/ItemCard.php
+++ app/Livewire/ItemCard.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class ItemCard extends Component
 {
+
+    public function placeholder()
+    {
+        return view('livewire.skeleton.item-skeleton');
+
+    }
     public function render()
     {
         return view('livewire.item-card');
